509806ef265102fec4851187ea4ae3b3ee738d43
oresnet <ngophuc993@gmail.com>
2020-01-16 20:21:24 -0800

    Java-ImgPix-1.0.0.1
    -Add function where change img to pixel value that is store in an 2D in array

--- src/ImgPro.java
+++ src/ImgPro.java
@@ -1,7 +1,10 @@
-import java.awt.image.BufferedImage;
+import java.awt.image.*;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.util.Arrays;
+
+
 
 public class ImgPro {
 
@@ -21,6 +24,52 @@ public class ImgPro {
 		return img;
 	}
 
+	
+	
+	
+	public int[][] convert2DImageToPixel(BufferedImage img){
+
+	      final byte[] pixels = ((DataBufferByte) img.getRaster().getDataBuffer()).getData();
+	      final int width = img.getWidth();
+	      final int height = img.getHeight();
+	      final boolean hasAlphaChannel = img.getAlphaRaster() != null;
+
+	      int[][] result = new int[height][width];
+	      if (hasAlphaChannel) {
+	         final int pixelLength = 4;
+	         for (int pixel = 0, row = 0, col = 0; pixel + 3 < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+	            argb += ((int) pixels[pixel + 1] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      } else {
+	         final int pixelLength = 3;
+	         for (int pixel = 0, row = 0, col = 0; pixel + 2 < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += -16777216; // 255 alpha
+	            argb += ((int) pixels[pixel] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 1] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
+	         }
+	      }
+	      
+	    return result;
+		
+	}
 
 
 }
--- src/main.java
+++ src/main.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.image.*;
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+import java.awt.event.*;
 
 
 
@@ -11,14 +14,19 @@ class main {
 		
 		
 		/**/
-		BufferedImage image = imgpro.getImg("C:/Users/Sero/Desktop/Software/Github/Java-ImgPix/test_1.png");
+		BufferedImage image = imgpro.getImg("C:/Users/Sero/Desktop/Software/Github/Java-ImgPix/test_2.jpg");
+		int[][] result = imgpro.convert2DImageToPixel(image);
 		
 		
+		System.out.println(result[0][0]);
+		/*
+	     for(int i = 0; i < result.length; i++) {
+	    	 System.out.println(Arrays.toString(result[i]));
+	     }
+		 */
 		
 		
-		
-		
-		
+
 		 JFrame editorFrame = new JFrame("Image Demo");
 		 
 		 ImageIcon imageIcon = new ImageIcon(image);
@@ -29,5 +37,12 @@ class main {
 	     editorFrame.pack();
 	     editorFrame.setLocationRelativeTo(null);
 	     editorFrame.setVisible(true);
+	     
+	     
+	
+
+
+	     
+	     
 	 }
 }
